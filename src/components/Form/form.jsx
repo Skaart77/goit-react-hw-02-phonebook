d972cdf8558e9d2fd55dab0dc9ffbc1c36edd051
Serhii Skliarov <114434801+Skaart77@users.noreply.github.com>
2023-05-04 15:43:13 +0300

    new update

--- src/components/Form/form.jsx
+++ src/components/Form/form.jsx
@@ -30,6 +30,7 @@ class Form extends Component {
           <Label>
             Name
             <Input
+              placeholder="Name"
               type="text"
               name="name"
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
@@ -42,6 +43,7 @@ class Form extends Component {
           <Label>
             Number
             <Input
+              placeholder="Number"
               type="tel"
               name="number"
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
